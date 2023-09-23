92cb4683abd5e4af82ef7a4d9569b9aade50b491
Basel85 <baselbasel1515@gmail.com>
2023-09-23 13:45:45 +0300

    b

---
+++ lib/core/app_assets.dart
@@ -0,0 +1,7 @@
+class AppAssets {
+  static const String homeIcon = 'assets/icons/home.svg';
+  static const String accountIcon = 'assets/icons/account.svg';
+  static const String searchIcon = 'assets/icons/search.svg';
+  static const String clockIcon = 'assets/icons/clock.svg';
+  static const String hospitalIcon = 'assets/icons/hospital.svg';
+}
