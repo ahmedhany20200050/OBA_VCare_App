07d110637993fa748be8e180831dc4864fc4d1d5
Basel85 <baselbasel1515@gmail.com>
2023-09-23 13:05:28 +0300

    tesy

--- lib/Features/home/presentation/views/home_screen.dart
+++ lib/Features/home/presentation/views/home_screen.dart
@@ -26,7 +26,7 @@ class _HomeScreenState extends State<HomeScreen> {
           builder: (_, __) => IndexedStack(
             index: _currentIndex,
             children: const [
-              
+               
             ],
           ),
         ),
