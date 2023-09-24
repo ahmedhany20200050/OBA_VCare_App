75c2a9c906d53ec1d187710f81e9baffec3b24af
Omar99323 <110248328+Omar99323@users.noreply.github.com>
2023-09-24 15:18:51 +0300

    booking select date

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        initialRoute: LoginScreen.id,
+        initialRoute: DoctorDetails.id,
         routes: {
           LoginScreen.id: (context) => const LoginScreen(),
           RegisterScreen.id: (context) => const RegisterScreen(),
