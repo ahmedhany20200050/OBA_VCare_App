567f3e06a4e3d809fc070be2f03587b11fd5d195
ahmed <ahmedhany20200050@gmail.com>
2023-09-24 13:24:23 +0300

    ggwp

--- lib/Features/Splash/presentation/views/splash_screen.dart
+++ lib/Features/Splash/presentation/views/splash_screen.dart
@@ -1,8 +1,13 @@
 // ignore_for_file: body_might_complete_normally_nullable
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:untitled/Features/OnBoarding/presentation/views/first_on_boarding_screen.dart';
+import 'package:untitled/Features/login/presentation/views/login_screen.dart';
 import 'package:untitled/core/app_styles.dart';
 import 'package:untitled/core/utils/size_config.dart';
+
+import '../../../home/presentation/views/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   static const id = 'SplashScreen';
@@ -18,8 +23,23 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    Future.delayed(const Duration(seconds: 3),(){
-      Navigator.of(context).pushNamed(FirstOnBoardingScreen.id);
+    Future.delayed(const Duration(seconds: 2),()async{
+      var storage =const FlutterSecureStorage();
+      String? token=await storage.read(key: "token");
+      final per = await SharedPreferences.getInstance();
+
+      bool? b=per.getBool("firstTime");
+      if(b==null){
+        Navigator.of(context).pushReplacementNamed(FirstOnBoardingScreen.id);
+        await per.setBool("firstTime", true);
+      }else{
+        if(token==null){
+          Navigator.of(context).pushReplacementNamed(LoginScreen.id);
+        }else{
+          Navigator.of(context).pushReplacementNamed(HomeScreen.id);
+        }
+      }
+
     } );
   }
   @override
